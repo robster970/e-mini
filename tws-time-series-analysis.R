9292da214f08c3831074771b957bd0f6d2eac75b
robster970 <robster970@gmail.com>
2015-11-22 21:45:56 +0000

    Small tweek

--- tws-time-series-analysis.R
+++ tws-time-series-analysis.R
@@ -14,7 +14,7 @@ c <- tail(sample.data, a/2)
 
 #Start the Plotting
 win.graph()
-par(mfrow=c(1,4))
+par(mfrow=c(1,2))
 
 #Plot histogram for B.Sample
 hist(b,30, prob=T, col="green")
@@ -23,6 +23,9 @@ lines(density(b, na.rm=TRUE), col="red", lwd=2)
 #Plot histogram for C.Sample
 hist(c,30, prob=T, col="yellow")
 lines(density(c, na.rm=TRUE), col="blue", lwd=2)
+
+win.graph()
+par(mfrow=c(1,3))
 
 #Plot PDFs for B & C Sample
 plot(density(b, na.rm=TRUE), col="red", lwd=2)
@@ -39,7 +42,13 @@ c.n <- nrow(c)
 plot(b.sorted, (1:b.n)/b.n, type = 's', ylim = c(0, 1), col="red", lwd=1, main="CDF for (b) and (c)")
 lines(c.sorted, (1:b.n)/b.n, type = 's', ylim = c(0, 1), col="blue", lwd=1)
 
+#Plot of B and C sample compared to straight line to see whether distributions are broadly equal
 
+diag.1 <- seq(min(coredata(sample.data)),max(coredata(sample.data)),0.1)
+diag.2 <- seq(min(coredata(sample.data)),max(coredata(sample.data)),0.1)
+
+plot(b.sorted~c.sorted, col="orange", lwd=2, main="b vs c sample")
+lines(diag.1~diag.2)
 
 
 
