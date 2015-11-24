ab02ca0fbc5df3e786d696068c76426ae987d850
robster970 <robster970@gmail.com>
2015-11-24 00:28:11 +0000

    Fixed bug with unequal sample lengths
    When the sample lengths are not equal, the final plot does not render.
    Fixed by making the shorter sample length the full set used for the
    larger sample. data set.

--- tws-time-series-analysis.R
+++ tws-time-series-analysis.R
@@ -39,15 +39,18 @@ b.n <- nrow(b)
 c.sorted <- sort(coredata(c))
 c.n <- nrow(c)
 
+if (b.n < c.n) {d.n <- b.n}
+if (c.n < b.n) {d.n <- c.n}
+
 plot(b.sorted, (1:b.n)/b.n, type = 's', ylim = c(0, 1), col="red", lwd=1, main="CDF for (b) and (c)")
-lines(c.sorted, (1:b.n)/b.n, type = 's', ylim = c(0, 1), col="blue", lwd=1)
+lines(c.sorted, (1:c.n)/c.n, type = 's', ylim = c(0, 1), col="blue", lwd=1)
 
 #Plot of B and C sample compared to straight line to see whether distributions are broadly equal
 
 diag.1 <- seq(min(coredata(sample.data)),max(coredata(sample.data)),0.1)
 diag.2 <- seq(min(coredata(sample.data)),max(coredata(sample.data)),0.1)
 
-plot(b.sorted~c.sorted, col="orange", lwd=2, main="b vs c sample")
+plot(b.sorted~head(c.sorted, d.n), col="orange", lwd=2, main="b vs c sample")
 lines(diag.1~diag.2)
 
 
