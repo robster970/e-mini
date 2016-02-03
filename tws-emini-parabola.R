0ccc10f7da3f503aa67bc1ae77c49b1f77991c1c
robster970 <robster970@gmail.com>
2016-02-03 07:19:14 +0000

    Updated for new short term trend

--- tws-emini-parabola.R
+++ tws-emini-parabola.R
@@ -232,8 +232,8 @@ iterations <- iterations+1
 # Comment this line out if running in real time.                       #
 ########################################################################
 
-target.start.date <- "2016-01-20 17:30:00"
-#target.start.date <- "2016-01-29 13:30:00"
+#target.start.date <- "2016-01-20 17:30:00"
+target.start.date <- "2016-02-01 20:30:00"
 mid.hourly.time.index <- as.vector(index(mid.hourly))
 start.point <- match(as.POSIXct(target.start.date), mid.hourly.time.index)
 
