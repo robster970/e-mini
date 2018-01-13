c8ce44de8e46e972a8aee1252bf784eaf8d7e95a
robster970 <robster970@gmail.com>
2018-01-13 23:04:25 +0000

    Ability to identify the date of the signal and clarify as a header above the data

--- sierra-ES-VIX-daily-merge.R
+++ sierra-ES-VIX-daily-merge.R
@@ -34,13 +34,18 @@ entryPDiffSift <- subset(entryNDistSift, entryNDistSift$PDiff.vixdata > -0.03 & 
 # Extract VIX and PDiff values and make numeric for subsequent manipulation
 lastVixEntry <- as.numeric(tail(entryPDiffSift$Last.vixdata,1))
 lastPDiffEntry <- as.numeric(tail(entryPDiffSift$PDiff.vixdata,1))
+lastEntryDate <- tail(entryPDiffSift[,0],1)
 
 exitNDistSift <- subset(cleanData, cleanData$NDist.vixdata < 0.159)
 exitPDiffSift <- subset(exitNDistSift, exitNDistSift$PDiff.vixdata > -0.03 & exitNDistSift$PDiff.vixdata < 0.03)
 
 if (lastPDiffEntry > 0 ) {
+cat("High NDist NOT confirmed on ")
+print(lastEntryDate)
+cat("by negative PDiff - wait for negative PDiff on day after signal\n")
 cleanData[which(cleanData$Last.vixdata == lastVixEntry) + c(1,0) ,]
 } else {
+cat("High NDist confirmed by negative PDiff\n")
 tail(entryPDiffSift, n=2)
 }
 
